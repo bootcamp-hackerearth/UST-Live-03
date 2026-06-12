0e88f9f17e9fd57d9e28e1eef407a5f5085b0532
ChrisJohns-UST <Chris.Johnson@ust.com>
2026-06-12 21:19:42 +0530

    Update SecurityConfig.java

--- POS/src/main/java/com/ust/pos/config/SecurityConfig.java
+++ POS/src/main/java/com/ust/pos/config/SecurityConfig.java
@@ -11,8 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -30,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) {
 
         http
-                .csrf(csrf -> csrf.disable()) // Disable for testing
+                .csrf(csrf -> csrf.disable()) 
                 .authorizeHttpRequests(auth -> auth
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
 
