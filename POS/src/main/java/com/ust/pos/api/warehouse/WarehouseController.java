3d7ad0e07a94d03f71dfbfc7947cc06db267da24
Rohit Raj Saji <rohitraj.saji@ust.com>
2026-06-26 17:02:14 +0530

    RBAC implemented across Warehouse Modules

--- POS/src/main/java/com/ust/pos/api/warehouse/WarehouseController.java
+++ POS/src/main/java/com/ust/pos/api/warehouse/WarehouseController.java
@@ -7,6 +7,7 @@ import com.ust.pos.warehouse.service.WarehouseService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class WarehouseController extends BaseController {
     }
 
     @PostMapping("/list")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<List<WarehouseDto>> list(@RequestBody PaginationDto paginationDto) {
         Pageable pageable = getPageable(paginationDto.getPage(), paginationDto.getSizePerPage(), paginationDto.getSortDirection(), paginationDto.getSortField());
         List<WarehouseDto> warehouses = warehouseService.findAll(pageable);
@@ -28,6 +30,7 @@ public class WarehouseController extends BaseController {
     }
 
     @GetMapping("/{identifier}")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<WarehouseDto> getByIdentifier(@PathVariable String identifier) {
         WarehouseDto response = warehouseService.findByIdentifier(identifier);
         if (response == null) {
@@ -37,6 +40,7 @@ public class WarehouseController extends BaseController {
     }
 
     @PostMapping("/save")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<WarehouseDto> create(@RequestBody WarehouseDto warehouseDto) {
         WarehouseDto response = warehouseService.save(warehouseDto);
         if (!response.isSuccess()) {
@@ -46,6 +50,7 @@ public class WarehouseController extends BaseController {
     }
 
     @PutMapping("/update/{identifier}")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<WarehouseDto> update(@PathVariable String identifier, @RequestBody WarehouseDto warehouseDto) {
         warehouseDto.setIdentifier(identifier);
         WarehouseDto response = warehouseService.update(warehouseDto);
@@ -56,6 +61,7 @@ public class WarehouseController extends BaseController {
     }
 
     @DeleteMapping("/delete/{identifier}")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<Boolean> delete(@PathVariable String identifier) {
         try {
             warehouseService.delete(identifier);
@@ -66,6 +72,7 @@ public class WarehouseController extends BaseController {
     }
 
     @PostMapping("/toggle/{identifier}")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<Boolean> toggleStatus(@PathVariable String identifier) {
         try {
             warehouseService.toggleStatus(identifier);
@@ -76,6 +83,7 @@ public class WarehouseController extends BaseController {
     }
 
     @GetMapping("/active")
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<List<WarehouseDto>> getActiveWarehouses() {
         List<WarehouseDto> warehouses = warehouseService.findIfTrue();
         return ResponseEntity.ok(warehouses);
--- POS/src/main/java/com/ust/pos/user/service/impl/PosUserDetailsService.java
+++ POS/src/main/java/com/ust/pos/user/service/impl/PosUserDetailsService.java
@@ -2,13 +2,19 @@ package com.ust.pos.user.service.impl;
 
 import com.ust.pos.dto.UserDto;
 import com.ust.pos.user.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class PosUserDetailsService implements UserDetailsService {
+
     private final UserService userService;
 
     public PosUserDetailsService(UserService userService) {
@@ -16,14 +22,21 @@ public class PosUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        UserDto userDto = userService.findByUserName(username);
-
+    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
+        UserDto userDto = userService.findByUserName(identifier);
         if (userDto == null) {
-            throw new UsernameNotFoundException("User not found: " + username);
+            throw new UsernameNotFoundException("User not found: " + identifier);
         }
 
-        return org.springframework.security.core.userdetails.User.withUsername(userDto.getUsername()).password(userDto.getPassword()).build();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for(String authority : userDto.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(authority));
+        }
+
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(userDto.getUsername())
+                .password(userDto.getPassword()).authorities(authorities)
+                .build();
     }
 }
