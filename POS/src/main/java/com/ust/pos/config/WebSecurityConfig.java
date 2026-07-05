c81d411f7a15646b15aae7a4e07af87ee31ef983
Harshavardhan-UST <308227@ust.com>
2026-07-05 20:56:30 +0530

    Restrict CORS to specific origin

--- POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
+++ POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://3.25.160.30:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
