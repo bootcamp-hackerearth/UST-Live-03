2704a832e4cb67132278f1be63bf96650b801f0a
Harshavardhan-UST <308227@ust.com>
2026-07-05 21:00:49 +0530

    Update CORS configuration to allow all origins

--- POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
+++ POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://3.25.160.30:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
