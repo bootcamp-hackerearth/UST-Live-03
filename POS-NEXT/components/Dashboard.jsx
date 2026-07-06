06d11b9f35276a38e1e5d2e39bda91b93fa2fe1d
pranavp <pranavp@users.noreply.github.com>
2026-07-06 17:56:52 +0530

    sprint-5 Github actions deployed -tested working ci/cd pipeline

--- POS-NEXT/components/Dashboard.jsx
+++ POS-NEXT/components/Dashboard.jsx
@@ -169,7 +169,7 @@ export default function DashboardPage() {
             <button onClick={() => router.push("/orders")}
               className="flex items-center gap-2 border border-violet-200 hover:bg-violet-50 px-5 py-3 rounded-xl transition">
               <Receipt size={18} />
-              View Orders+++
+              View Orders+++ please
             </button>
           </div>
         </div>
