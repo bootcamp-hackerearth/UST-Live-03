a8282ae7d30334c001ba69b83f7927ecb1e21108
ardra-ah <ardra.ah@ust.com>
2026-07-06 10:53:52 +0530

    Changed baseURL for deployment

--- POS-FRONTEND/app/register/page.jsx
+++ POS-FRONTEND/app/register/page.jsx
@@ -35,7 +35,7 @@ function Register() {
 		const fetchRoles = async () => {
 			try {
 				const response = await axios.get(
-					"http://localhost:8080/api/role/all"
+					"http://16.176.196.182:8080/api/role/all"
 				);
 
 				setRoles(response.data || []);
