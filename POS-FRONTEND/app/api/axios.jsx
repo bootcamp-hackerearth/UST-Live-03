b440d32c0d2c0fe5d4505aae04f1f0188e331cbf
Kushal S <kushal.s@ust.com>
2026-07-06 22:24:23 +0530

    Change baseURL to relative path in deployement for API

--- POS-FRONTEND/app/api/axios.jsx
+++ POS-FRONTEND/app/api/axios.jsx
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
