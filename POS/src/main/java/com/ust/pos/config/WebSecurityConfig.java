dbb828a9922ddb21b1c81ecde3ba62c51737dfd8
HemanthUst2215 <308279@ust.com>
2026-07-06 16:35:27 +0530

    Update WebSecurityConfig.java

--- POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
+++ POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
@@ -101,7 +101,7 @@ public class WebSecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:*")); // ✅ FIX
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*","http://15.135.137.125:3000")); // ✅ FIX
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // ✅ FIX
         configuration.setAllowCredentials(true);
