605668da9710635c53a40cbe69105ff6a5b6f7af
ardra-ah <ardra.ah@ust.com>
2026-07-06 12:00:57 +0530

    Changed baseURL for deployment

--- POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
+++ POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
@@ -95,7 +95,7 @@ public class WebSecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        configuration.setAllowedOriginPatterns(List.of("http://16.176.196.182:*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
