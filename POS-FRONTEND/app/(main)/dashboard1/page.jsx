aedb85332097ac47f05d09f02e592d3a027bf826
Anudeepika26 <308347@ust.com>
2026-07-07 14:55:33 +0530

    Update API endpoint to use relative URL

--- POS-FRONTEND/app/(main)/dashboard1/page.jsx
+++ POS-FRONTEND/app/(main)/dashboard1/page.jsx
@@ -24,7 +24,7 @@ const Dashboard1 = () => {
         }
 
         const res = await axios.get(
-          `http://localhost:8080/api/user/get?username=${username}`,
+          `/api/user/get?username=${username}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
