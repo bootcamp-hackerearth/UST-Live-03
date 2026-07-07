86e91539d1e61840ed3c5ad35cd9060670894d84
RitikaKumari-UST <Ritika.Kumari@ust.com>
2026-07-07 15:26:49 +0530

    Update api.js

--- POS-FRONTEND/services/api.js
+++ POS-FRONTEND/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
  
 const api = axios.create({
-  baseURL: "http://3.26.0.50:8080",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
