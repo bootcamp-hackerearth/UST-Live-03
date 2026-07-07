266031cda91aa81fdf49f6befa1e747430f9814d
HemanthUst2215 <308279@ust.com>
2026-07-07 09:42:32 +0530

    Update registration API endpoint URL

--- POS-FRONTEND/app/register/page.js
+++ POS-FRONTEND/app/register/page.js
@@ -60,7 +60,7 @@ function Register() {
     }
 
     try {
-      const res = await axios.post("http://15.135.137.125:8080/register", {
+      const res = await axios.post("http://3.25.202.80:8080/register", {
         username,
         name,
         phoneNo,
