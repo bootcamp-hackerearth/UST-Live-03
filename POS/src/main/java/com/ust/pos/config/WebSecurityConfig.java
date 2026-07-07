575ae16cb9c24f2968e5f7bdf91606e5def7cce3
SkyRanjan <akashranjan.lko@gmail.com>
2026-07-07 17:36:43 +0530

    cors fixed in WebSecurityConfig

--- POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
+++ POS/src/main/java/com/ust/pos/config/WebSecurityConfig.java
@@ -80,9 +80,9 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173")); // Allow the specific origin
+        configuration.setAllowedOriginPatterns(List.of("*")); // Allow the specific origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Allow credentials if needed
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // Apply CORS settings to all paths
