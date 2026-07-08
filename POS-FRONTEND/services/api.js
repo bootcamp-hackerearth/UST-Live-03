b2034e45025b7983c4c5ce1582a2bd7503958746
HemanthUst2215 <308279@ust.com>
2026-07-08 12:22:13 +0530

    Update api.js

--- POS-FRONTEND/services/api.js
+++ POS-FRONTEND/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://3.25.202.80:8080",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
